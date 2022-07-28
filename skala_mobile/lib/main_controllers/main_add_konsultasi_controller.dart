13c7676dc874d523bbf5330fd6fe253f46e70ea1
thefvnday <fandy.09.ahmad@gmail.com>
2022-07-28 23:43:45 +0700

    fix: go back not working

--- skala_mobile/lib/main_controllers/main_add_konsultasi_controller.dart
+++ skala_mobile/lib/main_controllers/main_add_konsultasi_controller.dart
@@ -1,23 +1,5 @@
-
-
-
 import 'package:get/get.dart';
-import 'dart:async';
 
-import 'package:get/get.dart';
-import 'package:skala_mobile/main_commons/main_constant_route.dart';
 class MainAddKonsultasiController extends GetxController{
-  Future<Timer> loadData() async {
-    return Timer(const Duration(seconds: 3), onDoneLoading);
-  }
-
-  void onDoneLoading() {
-    Get.offNamed(MainConstantRoute.mainAddKonsultasi);
-  }
-
-  @override
-  void onInit() {
-    loadData();
-    super.onInit();
-  }
+  
 }
--- skala_mobile/lib/main_controllers/main_edit_profile_controller.dart
+++ skala_mobile/lib/main_controllers/main_edit_profile_controller.dart
@@ -1,19 +1,5 @@
-import 'dart:async';
-
 import 'package:get/get.dart';
-import 'package:skala_mobile/main_commons/main_constant_route.dart';
+
 class MainEditProfileController extends GetxController {
-  Future<Timer> loadData() async {
-    return Timer(const Duration(seconds: 3), onDoneLoading);
-  }
 
-  void onDoneLoading() {
-    Get.offNamed(MainConstantRoute.mainEditProfile);
-  }
-
-  @override
-  void onInit() {
-    loadData();
-    super.onInit();
-  }
 }

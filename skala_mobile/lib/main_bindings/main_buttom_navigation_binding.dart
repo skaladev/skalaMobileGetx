a43de85ac05e3191c11cf291686f2343ef86acc7
thefvnday <fandy.09.ahmad@gmail.com>
2022-07-29 22:29:38 +0700

    fix controller not found

--- skala_mobile/lib/main_bindings/main_buttom_navigation_binding.dart
+++ skala_mobile/lib/main_bindings/main_buttom_navigation_binding.dart
@@ -1,10 +1,11 @@
-
 import 'package:get/instance_manager.dart';
+import 'package:skala_mobile/main_controllers/main_app_controller.dart';
 import 'package:skala_mobile/main_controllers/main_bottom_navigation_controller.dart';
 
-class MainButtomNavigationBinding extends Bindings{
- @override
-  void dependencies(){
-     Get.put(MainBottomNavigationController());
+class MainButtomNavigationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(MainAppController());
+    Get.put(MainBottomNavigationController());
   }
 }
--- skala_mobile/lib/main_bindings/main_login_binding.dart
+++ skala_mobile/lib/main_bindings/main_login_binding.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
+import 'package:skala_mobile/main_controllers/main_hive_controller.dart';
 import 'package:skala_mobile/main_controllers/main_login_controller.dart';
+import 'package:skala_mobile/main_controllers/main_welcome_controller.dart';
 
 
 class MainLoginBinding implements Bindings {
   @override
   void dependencies() {
+    Get.put(MainHiveController());
+    Get.put(MainWelcomeController());
     Get.put(MainLoginController());
   }
 }

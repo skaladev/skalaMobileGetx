7eb818f030f61a736cb1a31d71492743744dba3b
thefvnday <fandy.09.ahmad@gmail.com>
2022-08-29 01:33:01 +0700

    add gerated plugin firebase

--- skala_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ skala_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus_macos
-import firebase_core
-import firebase_messaging
 import path_provider_macos
 import share_plus_macos
 import sqflite
@@ -15,8 +13,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
